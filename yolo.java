9aebedefd1c44c228f24c90d2467c018ec4c6fc7
arkaprabhadey <euphoricdey420@gmail.com>
2020-09-22 14:55:40 +0530

    [orko Added: Changes]

--- yolo.java
+++ yolo.java
@@ -1,23 +1,30 @@
-public class Employee2{
+public class Employee3{
+	static final int IS_FULL_TIME=2;
+	static final int EMP_RATE_HR=20;
+	static final int IS_PART_TIME=1;
+
 	
 public static void main(String[] args) {
 	
-	int IS_FULL_TIME=1;
+	/*int IS_FULL_TIME=2;
 	int EMP_RATE_HR=20;
+	int IS_PART_TIME=1;*/
 
 	int employeewage=0;
 	int employeehrs=0;
 
-	double empcheck= Math.floor(Math.random()*10)%2;
+	double empcheck= Math.floor(Math.random()*10)%3;
 
 	if(empcheck==IS_FULL_TIME)
 	{
 		employeehrs=8;
 	}
-	/*else
+	else if(empcheck==IS_PART_TIME)
 	{
+		employeehrs=4;
+	}
+	else
 		employeehrs=0;
-	}*/
 	employeewage=EMP_RATE_HR*employeehrs;
 
 	System.out.println("Emp wage is: "+employeewage);
