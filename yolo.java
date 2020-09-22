187877cdb8254bfba774ae527fcd96939cdb6c53
arkaprabhadey <euphoricdey420@gmail.com>
2020-09-22 14:47:29 +0530

    [ok]

--- yolo.java
+++ yolo.java
@@ -1,18 +1,26 @@
-
-public class Employee{
+public class Employee2{
 	
 public static void main(String[] args) {
 	
 	int IS_FULL_TIME=1;
+	int EMP_RATE_HR=20;
+
+	int employeewage=0;
+	int employeehrs=0;
 
 	double empcheck= Math.floor(Math.random()*10)%2;
 
 	if(empcheck==IS_FULL_TIME)
 	{
-		System.out.println("Employee present");
+		employeehrs=8;
 	}
-	else
-		System.out.println("Employee absent");
+	/*else
+	{
+		employeehrs=0;
+	}*/
+	employeewage=EMP_RATE_HR*employeehrs;
+
+	System.out.println("Emp wage is: "+employeewage);
 }
 
 }
