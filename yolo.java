5c25c22dfa1c7d78acb91a0a16a60549c0cf62c2
arkaprabhadey <deyarkaprabha7@gmail.com>
2020-09-22 14:40:18 +0530

    [orko]

---
+++ yolo.java
@@ -0,0 +1,18 @@
+
+public class Employee{
+	
+public static void main(String[] args) {
+	
+	int IS_FULL_TIME=1;
+
+	double empcheck= Math.floor(Math.random()*10)%2;
+
+	if(empcheck==IS_FULL_TIME)
+	{
+		System.out.println("Employee present");
+	}
+	else
+		System.out.println("Employee absent");
+}
+
+}
